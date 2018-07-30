539ad46dd8dcf53a5efa6f28b6f752f4cd73a752
Dustin Lacewell <dlacewell@gmail.com>
2018-07-29 23:46:46 -0500

    Fix build scripts

--- build.el
+++ build.el
@@ -40,10 +40,24 @@
                "--abbrev=0"
                "HEAD")))))
 
-(defun to-elisp (file-name &optional keep-export)
+(defun destination-from-source (source ext)
+  (let* ((expanded (expand-file-name source))
+         (sans (file-name-sans-extension expanded))
+         (file-name (format "%s.%s" sans ext)))
+    file-name))
 
-  (interactive)
+;; (destination-from-source "~/org/dnd/character-sheet.org" "el")
 
+(defun render-from-source (source)
+  (let* ((expanded (expand-file-name source))
+         (sans (file-name-sans-extension expanded))
+         (extension (file-name-extension source))
+         (file-name (format "%s.rendered.%s" sans extension)))
+    file-name))
+
+;; (render-from-source "~/org/dnd/character-sheet.org")
+
+(defun install-org ()
   (use-package org
     :mode ("\\.org\\'" . org-mode)
     :config
@@ -52,45 +66,35 @@
     (require 'org)
     (require 'ox-org)
     (defalias #'org-git-version #'fix-org-git-version)
-    (defalias #'org-release #'fix-org-release)
+    (defalias #'org-release #'fix-org-release)))
 
-    ;; Auto-fill paragraphs
-    (add-hook 'org-mode-hook 'turn-on-auto-fill))
+(defun export (source ext cont &optional destination keep-export)
+  (install-org)
+  (let* ((source (expand-file-name source))
+         (render (render-from-source source))
+         (destination (or destination (destination-from-source source ext))))
+    (find-file source)
+    (funcall cont source render destination)
+    (when (and (file-exists-p render) (not keep-export))
+      (delete-file render))))
 
-  (let* ((outline-file-name (expand-file-name file-name))
-         (base-file-name (file-name-sans-extension outline-file-name))
-         (output-file-name (format "%s.export.org" base-file-name))
-         (elisp-file-name (format "%s.el" base-file-name)))
-    (find-file outline-file-name)
-    (org-export-to-file 'org output-file-name)
-    (org-babel-tangle-file output-file-name elisp-file-name)
-    (unless keep-export (delete-file output-file-name))))
+(defun do-elisp (source render destination)
+  (org-export-to-file 'org render)
+  (org-babel-tangle-file render destination 'emacs-lisp))
+
+(defun to-elisp (source &optional destination keep-export)
+  (interactive)
+  (export source "el" 'do-elisp destination keep-export))
+
+;; (to-elisp (expand-file-name "~/src/emacs-nougat/user-outlines/ldlework.org"))
 
 ;; TODO refactor these ^ v
 
-(defun to-html (file-name &optional keep-export)
+(defun do-html (source render destination)
+  (org-export-to-file 'org output-file-name)
+  (find-file output-file-name)
+  (org-export-to-file 'html html-file-name))
 
+(defun to-html (source &optional destination keep-export)
   (interactive)
-  (use-package htmlize)
-  (use-package org
-    :mode ("\\.org\\'" . org-mode)
-    :config
-    ;; This forces straight to load the package immediately in an attempt to avoid the
-    ;; Org that ships with Emacs.
-    (require 'org)
-    (require 'ox-org)
-    (defalias #'org-git-version #'fix-org-git-version)
-    (defalias #'org-release #'fix-org-release)
-
-    ;; Auto-fill paragraphs
-    (add-hook 'org-mode-hook 'turn-on-auto-fill))
-
-  (let* ((outline-file-name (expand-file-name file-name))
-         (base-file-name (file-name-sans-extension outline-file-name))
-         (output-file-name (format "%s.export.org" base-file-name))
-         (html-file-name (format "%s.html" base-file-name)))
-    (find-file outline-file-name)
-    (org-export-to-file 'org output-file-name)
-    (find-file output-file-name)
-    (org-export-to-file 'html html-file-name)
-    (unless keep-export (delete-file output-file-name))))
+  (export source "html" 'do-html destination keep-export))
